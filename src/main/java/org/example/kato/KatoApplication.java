9f85a216747f19c81e8913dd43b5009e7efeff04
AO-5002 <ao2005ortiz@gmail.com>
2026-02-07 22:48:43 -0600

    git init

---
+++ src/main/java/org/example/kato/KatoApplication.java
@@ -0,0 +1,13 @@
+package org.example.kato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KatoApplication.class, args);
+    }
+
+}
